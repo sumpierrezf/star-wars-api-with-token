4abddd0111661b8943ce8f9bdec08a313e817f9a
sumpierrezf <viqui09umpierrez@gmail.com>
2023-01-30 18:04:58 +0000

    Agregando response

--- src/app.py
+++ src/app.py
@@ -99,8 +99,11 @@ def protected():
     # Access the identity of the current user with get_jwt_identity
     current_user = get_jwt_identity()
     get_user = User.query.filter_by(email=current_user).first()
-    print(get_user.serialize())
-    return jsonify(logged_in_as=current_user), 200
+    response_body = {
+        "msg": "ok",
+        "user": get_user.serialize()
+    }
+    return jsonify(response_body), 200
 
 #Aca traemos todos los characters
 @app.route('/characters', methods=['GET'])
